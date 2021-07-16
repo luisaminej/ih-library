262a4ffd2af07b2b19ad1448becf38ea790634b6
Luisa Mine <luisaminj@gmail.com>
2021-07-16 13:17:57 -0500

    Hecho

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
 const express           = require('express')
 const app               = express()
 const mongoose          = require("mongoose")
+const Book = require('./models/Book')
 
 const book              = require('./models/Book')
 
 //MIDDLEWARES
-require('dotenv').config()
+
+require('dotenv').config() //VA PRIMERO
+
+mongoose.connect(process.env.MONGODB, {
+    useCreateIndex:true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {console.log("Conectados a MongoDB")})
+    .catch((e) => console.log(e))
 
 app.use(express.static('public'))
 app.set("view engine", "hbs")
@@ -19,8 +29,35 @@ app.set("view engine", "hbs")
 app.get("/", (req, res) => {
     res.render("index")
 })
+
+app.get("/books", (req, res) => {
+
+    Book.find({})
+        .then((LibrosEcontrados) => {
+            console.log(LibrosEcontrados)  // [{..}, {}..]si es un arreglo de objetos            res.render("books")
+                res.render("books",{
+                    libros: LibrosEcontrados  //Crear este
+                })
+        })
+        .catch((e) => {
+            console.log(e)})
+})
+
+
+app.get("/books/:bookId", (req, res) => {
+    console.log("Este es el req.params:", req.params)
+    const { bookId } = req.params
+    Book.findById(bookId)
+        .then(singleBook => {
+            console.log(`Libro encontrado:`, singleBook)
+            res.render("singleBook", {
+                libro: singleBook
+            })        
+        })
+        .catch(e => console.log(e))
+})
 // SERVIDOR
 
 app.listen(process.env.PORT, () => {
-
+ console.log("Servidor conectado")
 })
